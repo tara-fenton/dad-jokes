1a8502d980a467370730e50379c25566b756cc3f
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-03-24 09:02:30 -0400

    api call to random

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import './App.css';
 import RandomJoke from './components/RandomJoke'
 
 class App extends Component {
+
+  componentDidMount() {
+    this.randomAPI()
+  }
+  
+  randomAPI() {
+    let api = 'https://icanhazdadjoke.com/?callback=foo'
+    fetch(api, 
+      { headers: {
+        'Accept': 'application/json'
+      }       
+    }
+    ).then(response => response.json())
+    .then(json => {
+      console.log(json)
+    }).catch(e => console.log(e))
+  }
   render() {
     return (
       <div className="App">
