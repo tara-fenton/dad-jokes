5955ffc9ba6a1d5d13d140ce6bd528abccb1a3b2
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-03-24 13:52:38 -0400

    add Nav to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import Nav from './Nav'
 function Header() {
 
     return (
         <header>
-            <div className="logo">Dad Jokes</div>
-            <div>Nav</div>
+            <div className="logo">
+                Dad Jokes
+            </div>
+            <div>
+                <Nav />
+            </div>
         </header>
     )
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+
+    return (
+        <nav>
+            <button>Search</button>
+            <button>Random</button>
+        </nav>
+    )
+}
+
+export default Nav
